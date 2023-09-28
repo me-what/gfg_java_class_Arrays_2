99e6f9506869011a1f40dc64207c051a6803c9dc
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-09-28 12:06:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Student[] arr = { new Student(111, "bbbb", "london"),
+                new Student(131, "aaaa", "nyc"),
+                new Student(121, "cccc", "jaipur") };
+
+        System.out.println("Unsorted");
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+
+        Arrays.sort(arr, 1, 2, new Sortbyroll());
+
+        System.out.println("\nSorted by rollno");
+        for (int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+    }
+}
